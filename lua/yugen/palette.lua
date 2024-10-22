ede3106ba652131adbe1cf0cc03eaa90b2ae8b33
Atechchatterjee <atechchatterjeee@gmail.com>
2024-10-22 09:22:13 +0530

    feat: tweaked primary color & keywords

--- lua/yugen/palette.lua
+++ lua/yugen/palette.lua
@@ -1,22 +1,24 @@
 local variants = {
-  main = {
-    placeholder = '#FFFF00',
-    color100 = '#FAFAFA',
-    color200 = '#D4D4D4',
-    color300 = '#A9A9A9',
-    color400 = '#696969',
-    color500 = '#505050',
-    color600 = '#303030',
-    color700 = '#151515',
-    color800 = '#000000',
-    primary = '#FFBE89',
-    success = '#7EAB8E',
-    warning = '#FFF2AF',
-    error = '#F57A7A',
-    none = 'none'
-  },
+	main = {
+		placeholder = "#FFFF00",
+		color100 = "#FAFAFA",
+		color200 = "#D4D4D4",
+		color300 = "#A9A9A9",
+		color400 = "#696969",
+		-- color400 = "#848484",
+		color500 = "#505050",
+		-- color500 = "#686868",
+		color600 = "#303030",
+		color700 = "#151515",
+		color800 = "#000000",
+		-- primary = "#EAD051",
+		primary = "#C9AF48",
+		success = "#7EAB8E",
+		warning = "#FFF2AF",
+		error = "#F57A7A",
+		none = "none",
+	},
 }
-
 
 local palette = {}
 
